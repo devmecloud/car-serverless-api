458ecc7afb4c1869427b8501b2d0847a6d63e077
khaitk <khaitkdev@gmail.com>
2025-04-06 12:23:01 +0700

    Edit code in function put

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -23,14 +23,12 @@ export const putItemHandler = async (event) => {
     const body = JSON.parse(event.body);
     const id = body.id;
     const name = body.name;
-    const year = body.year;
-    const country = body.country;
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
     var params = {
         TableName : tableName,
-        Item: { id : id, name: name, year :year, country: country }
+        Item: { id : id, name: name }
     };
 
     try {
